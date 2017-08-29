485c71ac62d38a1391a51020d5ccd496fe9ac16f
elishaking <ek.chibueze@gmail.com>
2017-08-29 10:45:36 +0100

    py3 update

--- polynize/polynomials.py
+++ polynize/polynomials.py
@@ -1,13 +1,21 @@
+from itertools import zip_longest
+
+
 class Polynomial:
+    def __init__(self, *args, **kwargs):
+        self.args = args[0]
+        if kwargs.__contains__("order"):
+            self.args = []
+            for i in range(kwargs["order"] + 1):
+                self.args.append(kwargs["coeff"] if i == 0 else 0)
 
-    from itertools import izip_longest
-
-    def nonz(self, arr):
-        '''
-        Function nonz accepts a list and iterates iterate through the list to pass a non-zero value in the list(used for just the first non-zero value
+    def nonz(self, arr: list):
+        """
+        Function nonz accepts a list and iterates iterate through the list to pass a non-zero value in the list(used
+        for just the first non-zero value
         :param arr: list
         :return: non-zero value
-        '''
+        """
         for i in range(len(arr)):
             if arr[i] == 0:
                 continue
@@ -15,54 +23,49 @@ class Polynomial:
                 return arr[i]
 
     @staticmethod
-    def augment(arr1, arr2):
-        '''
+    def augment(arr1: list, arr2: list) -> list:
+        """
         :param arr1:list
         :param arr2:list
         :return:appends zero to the beginning of the list of smaller size before subtracting dividend from remainder
-        '''
-        for i in range(len(arr2)-len(arr1)):
+        """
+        for i in range(len(arr2) - len(arr1)):
             if arr2[i] == 0:
                 arr1 = [0] + arr1
             else:
                 break
         return arr1
 
-    def add_or_subtract(self, arr1, arr2, num):
-        '''
+    def add_or_subtract(self, arr1: list, arr2: list, num: int):
+        """
         :param arr1: list
         :param arr2: list
         :param num: constant to tell add_or_subtract function to add or subtract
         :return: arr1 + arr2 or arr1 - arr2
-        '''
+        """
         # If statement to determine the smaller array(or list) between self.args and other.args
         if len(arr1) < len(arr2):
-            minarr = arr1
-            maxarr = arr2
+            min_arr = arr1
+            max_arr = arr2
         else:
-            minarr = arr2
-            maxarr = arr1
-        minarr = self.new_array(len(minarr), len(maxarr),minarr)
-        result = []  #result is an array used to store the result
-        for i in range(len(maxarr)):
-            result.append(minarr[i] + maxarr[i]) if num == 0 else result.append(minarr[i] - maxarr[i])
+            min_arr = arr2
+            max_arr = arr1
+        min_arr = self.new_array(len(min_arr), len(max_arr), min_arr)
+        result = []  # result is an array used to store the result
+        for i in range(len(max_arr)):
+            result.append(min_arr[i] + max_arr[i]) if num == 0 else result.append(min_arr[i] - max_arr[i])
 
         return Polynomial(*result)
 
-    def __init__(self, *args, **kwargs):
-        self.args = args
-        if kwargs.__contains__("order"):
-            self.args = []
-            for i in range(kwargs["order"]+1):
-                self.args.append(kwargs["coeff"] if i == 0 else 0)
 
-    def new_array(self, min_len, max_len, arr):
-        '''
+    @staticmethod
+    def new_array(min_len: int, max_len: int, arr: list) -> list:
+        """
         :param min_len: length of small list
         :param max_len: length of big list
         :param arr: small list
         :return: new list equal in size with big list by appending zero to beginning of big list
-        '''
+        """
         new_len = max_len - min_len
         new_arr = list(arr)
         for i in range(new_len):
@@ -73,13 +76,13 @@ class Polynomial:
         self.add_or_subtract(self.args, other.args, 0)
 
     def __str__(self):
-        return str(self.args)
+        return str('Polynomial({0})'.format(self.args))
 
     def __len__(self):
         return len(self.args)
 
     @staticmethod
-    def multiply_polynomial(cp1, op1, cp2, op2):
+    def multiply_polynomial(cp1: float, op1: float, cp2: float, op2: float):
         return Polynomial(coeff=cp1 * cp2, order=op1 + op2)
 
     def __mul__(self, other):
@@ -96,7 +99,7 @@ class Polynomial:
         i = 0
         quotient = []
         dividend = self.args
-        for k in range(len(self.args)-1):
+        for k in range(len(self.args) - 1):
             if all(a == 0 for a in dividend):
                 break
             else:
@@ -104,10 +107,17 @@ class Polynomial:
                 current_quotient = self.nonz(dividend) / other.args[i]
                 current_remainder = [a * current_quotient for a in other.args]
                 current_remainder = self.augment(current_remainder, dividend)
-                dividend = [x - y for x, y in Polynomial.izip_longest(dividend, current_remainder, fillvalue=0)]
+                dividend = [x - y for x, y in zip_longest(dividend, current_remainder, fillvalue=0)]
                 quotient = tuple(quotient)
                 dividend = tuple(dividend)
                 return [quotient, dividend]
 
     def __sub__(self, other):
         self.add_or_subtract(self.args, other.args, 1)
+
+if __name__ == '__main__':
+    x = Polynomial([10, 10])
+    x2 = Polynomial([0])
+    print(x + x)
+    print(type(x.args))
+    print(x)
